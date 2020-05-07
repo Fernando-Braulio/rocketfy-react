6ee9a0d1e54dd0f432ce59039936f4e452ec5c4e
Fernando-Braulio <fernandobraulio@outlook.com.br>
2020-05-07 17:29:16 -0300

    add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <GlobalStyle />
   );
 }
 
