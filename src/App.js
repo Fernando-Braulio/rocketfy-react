23951aa6d70526856ebd47b44a0ca00014416f92
Fernando-Braulio <fernandobraulio@outlook.com.br>
2020-05-11 12:14:04 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
+import Header from './components/Header';
 
 function App() {
   return (
-    <GlobalStyle />
+    <>
+      <GlobalStyle />
+      <Header />
+    </>
   );
 }
 
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,5 +1,14 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    height: 80px;
+    padding: 0 30px;
+    background: #16b685;
+    color: #fff;
+    display: flex;
+    align-items: center;
 
+    -moz-box-shadow: 0 4px 4px rgba(0, 0, 0, 0.4);
+    -webkit-box-shadow: 0 4px 4px rgba(0, 0, 0, 0.4);
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.4);
 `;
