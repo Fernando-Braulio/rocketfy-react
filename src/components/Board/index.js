e4aefeb792beac7a5f606e1d6e916f127a934a26
Fernando-Braulio <fernandobraulio@outlook.com.br>
2020-05-15 12:51:00 -0300

    criando o context

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { loadLists } from '../../services/api';
 
 import List from '../List';
 import { Container } from './styles';
 
-const lists = loadLists();
+const data = loadLists();
 
 const Board = () => {
+  const [lists, setLists] = useState(data);
+
+  function move(from, to){
+    console.log(from, to);
+  }
+
   return (
     <Container>
       {lists.map(list => <List key={list.title} data={list} />)}
